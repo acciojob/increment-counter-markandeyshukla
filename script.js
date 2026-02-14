acb9ba520bb9ff56d97fbd9cd8bf68fba4e2fd12
Markandey Shukla <117027551+markandeyshukla@users.noreply.github.com>
2026-02-15 02:09:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let input=document.getElementById("incrementBtn");
 let counter=document.getElementById("counter")
 input.value=0;
-input.addEventListner("click",()=>{
+input.addEventListener("click",()=>{
 	alert(input.value);
 	input.value++;
 	counter.textContent = input.value;
